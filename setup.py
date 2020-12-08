2a906e4c632e0cd0bb92cf4ba1a061ad16f3ca85
deedy5 <deedy-ru@yandex.ru>
2020-12-08 07:42:25 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(name='requestsTor',
   version='0.1',
   description='Wrapper of the requests and stem libraries to make requests through TOR',
   url='https://github.com/deedy5/requestsTor',
   author='deedy5',
-  author_email='none@none.com',
+  author_email='deedy-ru@ya.ru',
+  long_description=long_description,
+  long_description_content_type="text/markdown",
   license='MIT',
   py_modules=['requestsTor'],
   install_requires=[
