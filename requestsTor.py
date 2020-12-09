0d018e974bcc5423ae602d913e4bfcecb292eccb
deedy5 <deedy-ru@yandex.ru>
2020-12-09 17:55:28 +0000

    Update requestsTor.py

--- requestsTor.py
+++ requestsTor.py
@@ -1,33 +1,83 @@
-from random import choice
+# v0.3
+from time import sleep
+from itertools import cycle
+from concurrent.futures import ThreadPoolExecutor
 import requests
 from stem import Signal
 from stem.control import Controller
 
 
 class requestsTor(object):
+    '''
+    If autochange_id = 5, Tor identity will be changed automatically
+    as soon as 5 urls will be downloaded through the one tor
+    socks port. So, if you have 5 socks ports, Tor identity
+    will be changed after downloading of 25 urls at all.
+    '''
     
-    def new_ip(self, cport=9151, password=None):
-        with Controller.from_port(port=cport) as controller:
-            controller.authenticate(password=password)
-            controller.signal(Signal.NEWNYM)
+    def __init__(self, tor_ports=[9150], tor_cport=9151, password=None, autochange_id=5, threads=None, debug=0):
+        self.tor_ports = cycle(tor_ports)
+        self.tor_ports_count = len(tor_ports)
+        self.tor_cport = tor_cport
+        self.password = password
+        self.headers = {"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+                        "Accept-Encoding": "gzip, deflate, br", "Accept-Language": "en-US,en;q=0.5",
+                        "Upgrade-Insecure-Requests": "1",
+                        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; rv:78.0) Gecko/20100101 Firefox/78.0"}
+        self.ip_api_list = cycle(['https://api.my-ip.io/ip', 'https://api.ipify.org', 'https://ifconfig.me/ip',
+                                  'https://icanhazip.com/', 'https://ipinfo.io/ip', 'https://whoer.net/ip',
+                                  'https://wtfismyip.com/text', 'https://checkip.amazonaws.com/',
+                                  'https://bot.whatismyipaddress.com',])
+        self.autochange_id = autochange_id
+        self.newid_counter = cycle(range(1, autochange_id+1)) if autochange_id else None
+        self.threads = threads
+        self.debug = debug
+    
+    def new_id(self):
+        with Controller.from_port(port=self.tor_cport) as controller:
+            controller.authenticate(password=self.password)
+            controller.signal(Signal.NEWNYM)            
+            if self.debug == 1:
+                print(f"\nTOR control port authenticated: {controller.is_authenticated()}")
+                print(f"TOR NEW IDENTITY\n")
+            sleep(3)
 
     def check_ip(self):
-        ip_api_list = ['https://api.my-ip.io/ip', 'https://api.ipify.org', 'https://ifconfig.me/ip', 
-                       'https://httpbin.org/ip', 'https://icanhazip.com/', 'https://ipinfo.io/ip',
-                       'https://wtfismyip.com/text', 'https://checkip.amazonaws.com/', 
-                       'https://bot.whatismyipaddress.com', 'https://whoer.net/ip',]
-        return self.get(choice(ip_api_list)).text
+        ip_api_url = next(self.ip_api_list)
+        return self.get(ip_api_url).text
 
-    def get(self, url, port=9150):
-        headers = {"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-                   "Accept-Encoding": "gzip, deflate, br", "Accept-Language": "en-US,en;q=0.5",
-                   "Upgrade-Insecure-Requests": "1",
-                   "User-Agent": "Mozilla/5.0 (Windows NT 10.0; rv:78.0) Gecko/20100101 Firefox/78.0"}
-        proxies = {'http': f'socks5h://localhost:{port}', 'https': f'socks5h://localhost:{port}',}
-        return requests.get(url, headers = headers, proxies = proxies)
+    def _fetch(self, url):
+        tor_port = next(self.tor_ports)
+        proxies = {'http': f'socks5h://localhost:{tor_port}', 'https': f'socks5h://localhost:{tor_port}',}
+        resp = requests.get(url, headers = self.headers, proxies = proxies)
+        if self.debug == 1:
+            print(f"proxy=localhost:{tor_port} status={resp.status_code} url={resp.url}")
+        return resp
+
+    def get(self, url):
+        resp = self._fetch(url)
+        if self.autochange_id and next(self.newid_counter) == self.autochange_id:
+            self.new_id()
+        return resp
+
+    def get_urls(self, urls):
+        results, temp_urls = [], []
+        step = self.tor_ports_count * self.autochange_id
+        for i,url in enumerate(urls, start=1):
+            temp_urls.append(url)
+            if i % step == 0:
+                temp_results = self._fetch_urls(temp_urls)
+                results.extend(temp_results)
+                temp_urls.clear()
+                self.new_id()
+                print(f'Progress: {i} urls')
+        temp_results = self._fetch_urls(temp_urls)
+        results.extend(temp_results)
+        return results
     
-    def __enter__(self):
-        return self
-    
-    def __exit__(self, *args):
-        return self
+    def _fetch_urls(self, urls):
+        results = []
+        with ThreadPoolExecutor(max_workers=self.threads) as executor:
+            for resp in executor.map(self._fetch, urls):
+                results.append(resp)             
+        return results
