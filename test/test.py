5c279842b4b0a704321639a3dc67888be7797c92
deedy5 <deedy-ru@yandex.ru>
2020-12-09 17:53:18 +0000

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,34 +1,35 @@
 from collections import Counter
 from requestsTor_test import requestsTor
 
-urls = (f'https://habr.com/ru/post/{x}' for x in range(531500, 531550))
-url = 'https://habr.com/ru/post/531622'
-##urls = (f'https://api.my-ip.io/ip' for x in range(531500, 531550))
+
+url = 'https://ya.ru'
+urls = (f'https://habr.com/ru/post/{x}' for x in range(5))
+urls2 = (f'https://api.my-ip.io/ip' for _ in range(50))
 
 def main():
-    rt = requestsTor(tor_ports=[9000, 9001, 9002, 9003, 9004])
-##    print(rt.check_ip())
-##    rt.new_ip()
-##    print(rt.check_ip())
-##    print(rt.get('https://r0.ru').text)
-##    print(rt.get('https://ya.ru').text)
-##    print(rt.get('https://ya.ru').text)
-##    print(rt.get('https://ya.ru').text)
-##    print(rt.get('https://ya.ru').text)
-##    print(rt.get('https://ya.ru').text)
-
-##    print(rt.check_ip())
-##    print(rt.check_ip())
-##    print(rt.check_ip())
-##    print(rt.check_ip())
-##    print(rt.check_ip())
+    rt = requestsTor(tor_ports=[9000, 9001, 9002, 9003, 9004], debug=1)
+    print(rt.check_ip())
     rt.new_id()
 
+    print(rt.get('https://ya.ru').text)
+    print(rt.get('https://ya.ru').text)
+    print(rt.get('https://ya.ru').text)
+    print(rt.get('https://ya.ru').text)
+    print(rt.get('https://ya.ru').text)
+    print(rt.get('https://ya.ru').text)
+    print(rt.get('https://ya.ru').text)
 
-##    res = rt.get_urls(urls)
-##    for r in res:
-##        print(r.url, r.text)
-##    print(Counter(r.text for r in res))
+
+
+    res = rt.get_urls(urls)
+    for r in res:
+        print(r.url, r.text)
+    print(Counter(r.text for r in res))
+
+    res2 = rt.get_urls(urls2)
+    for r in res2:
+        print(r.url, r.text)
+    print(Counter(r.text for r in res2))
     
 
     
