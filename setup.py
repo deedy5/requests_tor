9132eb6d27e500b28c9fa2b510bfe8418cdd3b3e
deedy5 <deedy-ru@yandex.ru>
2020-12-09 19:18:08 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,17 +4,18 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(name='requestsTor',
-  version='0.2',
-  description='Wrapper of the requests and stem libraries to make requests through TOR',
-  url='https://github.com/deedy5/requestsTor',
-  author='deedy5',
-  author_email='deedy-ru@ya.ru',
-  long_description=long_description,
-  long_description_content_type="text/markdown",
-  license='MIT',
-  py_modules=['requestsTor'],
-  install_requires=[
-    'requests>=2.25.0',
-    'stem>=1.8.0'
-  ],
-  zip_safe=False)
+    version='0.3',  
+    author='deedy5',
+    author_email='deedy-ru@ya.ru',
+    description='Requests via TOR with automatic TOR new identity. Downloading of urls list concurrently. Wrapper of the requests and stem libraries.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/deedy5/requestsTor',
+    license='MIT',
+    py_modules=['requestsTor'],
+    install_requires=['requests>=2.25.0', 'stem>=1.8.0'],
+    classifiers=["Programming Language :: Python :: 3",
+                 "License :: OSI Approved :: MIT License",
+                 "Operating System :: OS Independent",],
+    python_requires='>=3.6',
+    zip_safe=False)
