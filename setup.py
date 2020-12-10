deed1cba37ecbe48931fec4f078752ad6cdf35fa
deedy5 <65482418+deedy5@users.noreply.github.com>
2020-12-10 15:17:48 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="requestsTor",
+    name="requests_tor",
     version="0.5",
     author="deedy5",
     author_email="deedy-ru@ya.ru",
     description="Multithreading requests via TOR with automatic TOR new identity",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/deedy5/requestsTor",
+    url="https://github.com/deedy5/requests_tor",
     license="MIT",
-    py_modules=["requestsTor"],
+    py_modules=["requests_tor"],
     install_requires=["requests>=2.25.0", "stem>=1.8.0"],
     classifiers=[
         "Programming Language :: Python :: 3",
