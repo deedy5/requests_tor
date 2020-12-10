ca4872cde47b37fa4d7291755a28613d01d5c308
deedy5 <65482418+deedy5@users.noreply.github.com>
2020-12-10 14:37:43 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,19 +3,23 @@ from setuptools import setup
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-setup(name='requestsTor',
-    version='0.4',  
-    author='deedy5',
-    author_email='deedy-ru@ya.ru',
-    description='Multithreading requests via TOR with automatic TOR new identity',
+setup(
+    name="requestsTor",
+    version="0.4",
+    author="deedy5",
+    author_email="deedy-ru@ya.ru",
+    description="Multithreading requests via TOR with automatic TOR new identity",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/deedy5/requestsTor',
-    license='MIT',
-    py_modules=['requestsTor'],
-    install_requires=['requests>=2.25.0', 'stem>=1.8.0'],
-    classifiers=["Programming Language :: Python :: 3",
-                 "License :: OSI Approved :: MIT License",
-                 "Operating System :: OS Independent",],
-    python_requires='>=3.6',
-    zip_safe=False)
+    url="https://github.com/deedy5/requestsTor",
+    license="MIT",
+    py_modules=["requestsTor"],
+    install_requires=["requests>=2.25.0", "stem>=1.8.0"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    zip_safe=False,
+)
